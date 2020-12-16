923e301e3e4eca9b70b44695beeb0aa92975f7df
674834420@qq.com <674834420@qq.com>
2020-12-16 16:42:44 +0800

    MSAA

---
+++ gen/exampleLoader.ts
@@ -0,0 +1,13 @@
+
+
+  export const helloTriangle = async () => (await import('../src/examples/helloTriangle'));
+  
+
+  export const helloCircle = async () => (await import('../src/examples/helloCircle'));
+  
+
+  export const drawIndexed = async () => (await import('../src/examples/drawIndexed'));
+  
+
+  export const msaa = async () => (await import('../src/examples/msaa'));
+  
